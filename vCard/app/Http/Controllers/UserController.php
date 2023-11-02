85a0bd439c8b60cc8e2fb2c46046ada7f883196b
Daniel Carias <dfcpcarias@gmail.com>
2023-11-02 20:57:10 +0000

    Login and Register Falta validar o email.

--- vCard/app/Http/Controllers/UserController.php
+++ vCard/app/Http/Controllers/UserController.php
@@ -4,24 +4,66 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if($user){
+            if (Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'status' => 'sucess',
+                    'message' => [
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ],
+                    
+                ]);
+            }else{
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Incorrect password'
+                ]);
+            }
+        }else{
+            return response()->json([
+                'status' => 'error',
+                'message' => 'The user does not exist'
+            ]);
+        }
+    }
+
     public function index()
     {
         return User::all();
     }
 
-        /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $user = User::where('email', $request->email)->first();
+        if(!$user){
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            return response()->json([
+                'status' => 'sucess',
+                'message' => [
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ],
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'The user already exists'
+        ]);
     }
 
     /**
--- vCard/routes/api.php
+++ vCard/routes/api.php
@@ -4,4 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+//Rotas específicas
+Route::post('login', [UserController::class, 'login']);
+
+
+
+
+//Rotas para CRUD
 Route::apiResource('users', UserController::class);
