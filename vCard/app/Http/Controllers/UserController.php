44de25bf3f59eb19e976df7028a7c3a954869fcb
Frederico Bento <fredbento39@gmail.com>
2023-11-08 19:02:38 +0000

    Auth Update
    vcards users cant login yet.

--- vCard/app/Http/Controllers/UserController.php
+++ vCard/app/Http/Controllers/UserController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
-use App\Models\UserPhoneNumber;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class UserController extends Controller
 {
--- vCard/app/Models/User.php
+++ vCard/app/Models/User.php
@@ -16,9 +16,5 @@ class User extends Authenticatable
     // {
     //     return $this->where('name', $name)->first();
     // }
-    public function phoneNumbers()
-    {
-        return $this->hasMany(UserPhoneNumber::class, 'user_id');
-    }
 
 }
