74e31db3b2a3f1d015c519b29c368eff45cbc7a4
Frederico Bento <fredbento39@gmail.com>
2023-12-08 16:34:57 +0000

    Refactor: Applied response and error services to TransactionsController

--- vCard/app/Http/Controllers/TransactionController.php
+++ vCard/app/Http/Controllers/TransactionController.php
@@ -10,19 +10,32 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use App\Models\Vcard;
 use App\Models\User;
+use App\Services\ErrorService;
+use App\Services\ResponseService;
 
 class TransactionController extends Controller
 {
+
+    protected $errorService;
+    protected $responseService;
+
+    public function __construct(){
+        $this->errorService = new ErrorService();
+        $this->responseService = new ResponseService();
+
+    }
+
     public function index()
     {
         $transactions = Transaction::orderBy('date', 'desc')->paginate(10);
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'All Transactions retrieved successfully',
-            'data' => $transactions,
-            'last' => $transactions->lastPage(),
-        ], 200); // HTTP 200 OK
+        // return response()->json([
+        //     'status' => 'success',
+        //     'message' => 'All Transactions retrieved successfully',
+        //     'data' => $transactions,
+        //     'last' => $transactions->lastPage(),
+        // ], 200); // HTTP 200 OK
+        return $this->responseService->sendWithDataResponse(200, "All Transactions retrieved successfully", ['transactions' => $transactions, 'last' => $transactions->lastPage()]);
     }
 
     public function show(string $query, Request $request){
@@ -32,11 +45,7 @@ class TransactionController extends Controller
         ]);
 
         if($validator->fails()){
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Validation failed',
-                'errors' => $validator->errors(),
-            ], 422); // HTTP 422 Unprocessable Entity
+            return $this->errorService->sendValidatorError(422, "Validation Failed", $validator->errors());
         }
 
         switch ($query){
@@ -46,36 +55,33 @@ class TransactionController extends Controller
             case Str::contains($query, '@'): //email
                 $phone = Vcard::where('email', $query)->select('phone_number');
                 $transactions = Transaction::where('vcard', $phone)->orderBy('datetime', 'desc');
-                
+
                 break;
             default: //name
                 $phone = Vcard::where('name', 'LIKE', '%' . $query . '%')->pluck('phone_number');
                 $transactions = Transaction::whereIn('vcard', $phone)->orderBy('datetime', 'desc');
                 break;
         }
-        
+
 
         //Get the query allready filtered by name or phone or email and filter by blocked
         if($request->type != 'all'){
             $transactions = $transactions->where('type', $request->type)->paginate(10);
         }else{
             $transactions = $transactions->paginate(10);
-        }       
-
-        if($transactions){
-            return response()->json([
-            'status' => 'success',
-            'message' => 'Transactions retrieved successfully',
-            'data' => $transactions,
-            'last' => $transactions->lastPage(),
-            ], 200); // HTTP 200 OK
         }
 
-        return response()->json([
-            'status' => 'error',
-            'message' => 'The vcard with that phone number does not have any transactions',
-        ]);
-        
+        if($transactions){
+            // return response()->json([
+            // 'status' => 'success',
+            // 'message' => 'Transactions retrieved successfully',
+            // 'data' => $transactions,
+            // 'last' => $transactions->lastPage(),
+            // ], 200); // HTTP 200 OK
+            return $this->responseService->sendWithDataResponse(200, "Transactions retrieved successfully", ['transactions' => $transactions, 'last' => $transactions->lastPage()]);
+        }
+        return $this->errorService->sendStandardError(404, "The vcard with that phone number does not have any transactions");
+
     }
 
     /*public function MyTransactionsType(Request $request){
@@ -100,7 +106,7 @@ class TransactionController extends Controller
         }else{
             $transactions = $vcard->transactions()->orderBy('datetime', 'desc')->paginate(10);
         }
-        
+
         return response()->json([
             $transactions,
             'last' => $transactions->lastPage(),
@@ -114,10 +120,12 @@ class TransactionController extends Controller
         $transactions = $vcard->transactions()->orderBy('datetime', 'desc')->paginate(10);
         // $transformedTransactions = TransactionResource::collection($transactions);
 
-        return response()->json([
-            $transactions,
-            'last' => $transactions->lastPage(),
-        ], 200); // HTTP 200 OK
+        // return response()->json([
+        //     $transactions,
+        //     'last' => $transactions->lastPage(),
+        // ], 200); // HTTP 200 OK
+
+        return $this->responseService->sendWithDataResponse(200, null, ["transactions" => $transactions, "last" => $transactions->lastPage()]);
 
     }
 
@@ -128,11 +136,7 @@ class TransactionController extends Controller
         ]);
 
         if($validator->fails()){
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Validation failed',
-                'errors' => $validator->errors(),
-            ], 422); // HTTP 422 Unprocessable Entity
+            return $this->errorService->sendValidatorError(422, "Validation Failed", $validator->errors());
         }
 
 
@@ -141,12 +145,13 @@ class TransactionController extends Controller
         }else{
             $transactions = Transaction::orderBy('datetime', 'desc')->paginate(10);
         }
-        
-        return response()->json([
-            $transactions,
-            'last' => $transactions->lastPage(),
-        ], 200); // HTTP 200 OK
 
+        // return response()->json([
+        //     $transactions,
+        //     'last' => $transactions->lastPage(),
+        // ], 200); // HTTP 200 OK
+
+        return $this->responseService->sendWithDataResponse(200, null, ['transactions' => $transactions, 'last' => $transactions->lastPage()]);
 
     }
 
